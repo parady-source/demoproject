cb78918f0ae049dc3c2d224e3fb85153ed396777
Kyan <parady0000@gmail.com>
2021-12-30 00:15:24 +0800

    Prepare for Investment Editing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -41,6 +41,7 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { CdkTableModule } from '@angular/cdk/table';
 import { ChartsModule } from 'ng2-charts';
@@ -102,6 +103,7 @@ const appRoutes: Routes = [
     MatTableModule,
     MatToolbarModule,
     MatTooltipModule,
+    MatProgressSpinnerModule,
     CdkTableModule,
     ChartsModule
   ],
