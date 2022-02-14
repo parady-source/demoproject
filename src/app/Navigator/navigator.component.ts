30a15ec5950c7e858faba8a95df2d1ea072b42cb
Kyan <parady0000@gmail.com>
2022-02-14 23:02:44 +0800

    Amend last commit

--- src/app/Navigator/navigator.component.ts
+++ src/app/Navigator/navigator.component.ts
@@ -52,7 +52,10 @@ export class NavigatorComponent {
     }, 10000);
     setInterval(() => {
       var d = new Date();
-      this.IndexTimer = d.getFullYear() + '/' + d.getMonth() + '/' + d.getDay() + ' ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds().toString().padStart(2, '0');
+      this.IndexTimer =
+        d.getFullYear().toString() + '/' + (d.getMonth() + 1).toString().padStart(2, '0') + '/' + d.getDate().toString().padStart(2, '0')
+        + ' '
+        + d.getHours().toString().padStart(2, '0') + ':' + d.getMinutes().toString().padStart(2, '0') + ':' + d.getSeconds().toString().padStart(2, '0');
     }, 1000);
   }
 
